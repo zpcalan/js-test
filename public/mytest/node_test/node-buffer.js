55fae1384ea545c08511dca7af40aca4e119ad64
zpcalan <1303359419@qq.com>
2016-12-02 18:48:05 +0800

    明确Buffer和TypedArray的关系

--- public/mytest/node_test/node-buffer.js
+++ public/mytest/node_test/node-buffer.js
@@ -18,14 +18,42 @@
 // 用于各种编码字符串的互相转化
 (function () {
     const buf1 = Buffer.from('hello world', 'ascii');
-    console.log(buf1.toString('hex'));
+    // console.log(buf1.toString('hex'));
     // 输出: 68656c6c6f20776f726c64
-    console.log(buf1.toString('base64'));
+
+    // console.log(buf1.toString('base64'));
     // 输出: aGVsbG8gd29ybGQ=
+
     const buf2=Buffer.from('aGVsbG8gd29ybGQ=','base64')
-    console.log(buf2.toString('hex'));
+    // console.log(buf2.toString('hex'));
     // 输出：68656c6c6f20776f726c64
+
     const buf3=Buffer.from('周培晨','utf8')
-    console.log(buf3.toString('base64'))
+    // console.log(buf3.toString('base64'))
     // 输出：5ZGo5Z+55pmo
+})();
+// Buffer类型和TypedArray的关系
+// 注意寻常x86 cpu都是小端 即低位数存到低位地址 计算机读取的时候也是从低位先读 数组也从低位先存 累积到高位
+(function () {
+    const arr = new Uint16Array(2);
+    arr[0] = 5000;
+    arr[1] = 4000;
+
+    const buf1 = Buffer.from(arr); 
+    // console.log(buf1);
+    // 只是复制了arr 并且'Buffers are also Uint8Array TypedArray instances' 说明Buffer也是Uint8Array的实例
+    // 那么每个单元就只会存储1个字节，即unsigned int 8位
+    // 输出：<Buffer 88 a0>
+
+    const buf2 = Buffer.from(arr.buffer); 
+    // console.log(buf2);
+    // 与arr共享内存空间 那么有足够的空间防止4个元素(对于Buffer即Uint8Array来说为4个元素)
+    // 输出：<Buffer 88 13 a0 0f> 注意小端：5000的低位0x88放在低地址，因此先读取0x88
+    
+    arr[1] = 6000;
+    console.log(buf1);
+    // 因为是复制的 在全新的内存地址
+    //  因此还是输出：<Buffer 88 a0>
+    console.log(buf2);
+    //共享内存，同时改变，输出：<Buffer 88 13 70 17>
 })();
